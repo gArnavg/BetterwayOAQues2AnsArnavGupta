afb1d599fcafb90f6a639576d723d4d37c3ef3db
gArnavg <117974343+gArnavg@users.noreply.github.com>
2026-01-04 13:18:33 +0530

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    id: 1,
+    title: "Fjallraven Backpack",
+    price: 109.95,
+    category: "men's clothing",
+    stock: 5
+  },
+  {
+    id: 2,
+    title: "Mens Casual T-Shirt",
+    price: 22.3,
+    category: "men's clothing",
+    stock: 0
+  }
+  // add remaining products here
+];
